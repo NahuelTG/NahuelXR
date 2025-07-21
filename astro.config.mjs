33c9a6728ec6eb91c4c238aa825feddc3e347d4b
NahuelTG <nahueltorrezg@gmail.com>
2025-07-21 16:52:33 -0400

    update: paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
    integrations: [tailwind()],
    site: "https://NahuelTG.github.io",
+
    vite: {
       css: {
          preprocessorOptions: {
